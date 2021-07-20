675b01d6da58e1dc3fff6df20d79a5ea290bfb50
rizkysaputra4 <62467810+rizkysaputra4@users.noreply.github.com>
2021-07-20 16:57:41 +0700

    hide top bar

--- app/src/main/java/com/training/intro/SplashScreen.kt
+++ app/src/main/java/com/training/intro/SplashScreen.kt
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
 import android.view.WindowManager
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
